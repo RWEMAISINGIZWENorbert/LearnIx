18002770b612d1a13064ca481f9d4f757611fd73
RWEMAISINGIZWENorbert <rwema916@gmail.com>
2025-11-06 00:17:27 +0200

    rmv .

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// const API_BASE_URL = 'https://learnix-backend-navy.vercel.app';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log(` API_BASE_URL: ${API_BASE_URL}`);
 
