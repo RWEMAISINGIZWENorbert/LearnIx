7984c907f545f4a51fbe8d8885460463ce160277
RWEMAISINGIZWENorbert <rwema916@gmail.com>
2025-11-24 20:42:19 +0200

    .

--- src/components/public_components/Not_Found_Page/Not_Found_page.jsx
+++ src/components/public_components/Not_Found_Page/Not_Found_page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Not_Found_page.css'
+import './Not_found_page.css'
 import { MdArrowForwardIos } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 
