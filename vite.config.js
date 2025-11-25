9302a3127d1af386625f19590c54671c9f0a26f5
RWEMAISINGIZWENorbert <rwema916@gmail.com>
2025-11-25 10:33:18 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  efine: {
-    'import.meta.env.API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL)
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  define: {
+    'import.meta.env.API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
+    'import.meta.env.BASE_URL': JSON.stringify('/')
   }
   // server: {
   //   host: true, // allows network access
